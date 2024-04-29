c9f81b3ca35a0a11019800a0f0250350cde7061d
erikgty43 <60478672+erikgty43@users.noreply.github.com>
2024-04-30 00:41:41 +0200

    Add files via upload

---
+++ web speeld/code/webjs.js
@@ -0,0 +1,4 @@
+function toggleContactDropdown() {
+    var dropdown = document.querySelector('.contact-dropdown');
+    dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+}
